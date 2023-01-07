b04220a5113fd1851d6d82cee32044e380847df1
mahiuddin22 <mahiuddin.noyon@gmail.com>
2023-01-07 16:04:34 +0600

    new project

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-let title = document.title = "DOM Tutorial";
+//simple to-do project using jabascript dom.
 
-// let headerElement = document.getElementById('header');
-// // headerElement.textContent = 'To-To Apps';
-//
-// let itemsUL = document.getElementById('items');
-// let items = itemsUL.getElementsByClassName('item');
-//
-// for ( let i = 0; i<items.length; i++){
-//     items[i].style.color = 'red';
-// }
 
-let lastItems = document.querySelectorAll('.item:nth-child(4)');
-
-for ( let element of lastItems){
-    element.style.backgroundColor = 'red';
-    element.style.color = '#fff';
-}
+let newTask = document.querySelector("#new-task");
